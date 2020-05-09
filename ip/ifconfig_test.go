189701d5ea4ad465b75523b57bc212f8827adf66
arekmano <arekmano@localhost.localdomain>
2020-05-09 00:19:41 -0400

    test is more organized now

--- ip/ifconfig_test.go
+++ ip/ifconfig_test.go
@@ -9,14 +9,21 @@ import (
 )
 
 func TestGetPublicIP(t *testing.T) {
+	// Test data
 	client := ip.NewIfConfigClient()
+
+	// Execute
 	result, err := client.GetPublicIP()
 	require.NoError(t, err)
 	resultIP := net.ParseIP(result)
 	require.NoError(t, err)
+
+	// Execute
 	result, err = client.GetPublicIP()
 	require.NoError(t, err)
 	resultIP2 := net.ParseIP(result)
 	require.NoError(t, err)
+
+	// Verify
 	require.Equal(t, resultIP, resultIP2)
 }
